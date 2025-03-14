87a07c5e610a633c8a7876f88cbbdf6d44d569f4
Tonasj <87764307+Tonasj@users.noreply.github.com>
2025-03-14 03:47:03 +0200

    New test for updated pipelines

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <h1>CD Pipeline test</h1>
       
       <div className="card">
